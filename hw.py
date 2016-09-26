cd5dc27df5e80cac3dba2b2ec0a906c57fde2b78
Volker Ankenbrand <vankenbrand@csc.com>
2016-09-26 15:23:50 +1000

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+#
+#	Python test frame
+#
+from sys import argv
+
+arg=argv[1:]
+i=len(arg)
+print "No of args: ",i
+
+for j in arg:
+  print "Argument: ",j
+ 
+print "Hello world\n\n\n"
+
+
